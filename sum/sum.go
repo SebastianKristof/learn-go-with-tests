5fcab66673eca0ed7ac4c3c213b1c4ac50903688
Sebastian Kristof <cem@qlik.com>
2018-08-01 11:14:38 -0400

    working SumAllTails

--- sum/sum.go
+++ sum/sum.go
@@ -9,11 +9,18 @@ func Sum(numbers []int) (sum int) {
 }
 
 func SumAll(numbersToSum ...[]int) (sums []int) {
-	// sums := make([]int, len(numbersToSum))
 	
 	for _, numbers := range numbersToSum {
 		sums = append(sums, Sum(numbers))
 	}
 	
+	return
+}
+
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers[1:]))
+	}
+
 	return
 }
