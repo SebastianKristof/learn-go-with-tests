1c5e51306f4031d713441d23a0136bb433ea56df
Sebastian Kristof <cem@qlik.com>
2018-08-01 15:59:23 -0400

    rectangle

---
+++ rectangle/rectangle.go
@@ -0,0 +1,14 @@
+package rectangle
+
+type Rectangle struct {
+	Width float64
+	Height float64
+}
+
+func Perimeter(rectangle Rectangle) (perimeter float64) {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func Area(rectangle Rectangle) (area float64) {
+	return rectangle.Width * rectangle.Height
+}
